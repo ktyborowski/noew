80306dd41ae2141872dca64297f0ea85287fc358
pixpack <kamilty10@gmail.com>
2022-04-03 17:24:36 +0200

    Status page styling

--- src/pages/status.py
+++ src/pages/status.py
@@ -1,7 +1,9 @@
-import streamlit as st
 import sqlite3
+
+import streamlit as st
+
 from config import settings
-import time
+
 
 def get_count(table):
     conn = sqlite3.connect(settings["database"])
@@ -19,14 +21,11 @@ def get_count(table):
 def write():
     st.write("# Status")
     st.write("Ta strona przedstawia aktualne statystyki oznaczeń.")
-    st.write("*Dane aktualizują się przy zmianie strony lub odświeżeniu przeglądarki.*")
+    st.caption("Dane aktualizują się przy zmianie strony lub odświeżeniu przeglądarki.")
     st.write("---")
-
 
     with st.spinner("Ładowanie..."):
         col1, col2, col3 = st.columns(3)
-        col1.metric(label="Oznaczono ✔️", value=get_count("results"))
-        col2.metric(label="Pominięto ❌", value=10)
+        col1.metric(label="Oznaczono ✔️", value=get_count("labeled"))
+        col2.metric(label="Pominięto ❌", value=get_count("skipped"))
         col3.metric(label="Oczekujących ⏳", value=get_count("input"))
-
-
