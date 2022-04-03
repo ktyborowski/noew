0fb434b011dd8d44778af019f10d37ad94e3f85f
pixpack <kamilty10@gmail.com>
2022-04-03 17:24:01 +0200

    formatting

--- src/auth.py
+++ src/auth.py
@@ -1,34 +1,33 @@
-import streamlit as st
 from hashlib import sha256
+
+import streamlit as st
+
 
 def check_password():
     """Returns `True` if the user had the correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        
+
         hash = sha256()
         hash.update(st.session_state["password"].encode("utf-8"))
 
         if hash.hexdigest() == st.secrets["password"]:
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
+            del st.session_state["password"]
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        # First run, show input for password.
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         st.error("😕 Password incorrect")
         return False
     else:
-        # Password correct.
         return True
